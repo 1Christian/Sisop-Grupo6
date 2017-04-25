d9f0a3244e203779b671bf128bd66b83dbca671a
Christian Torres <christian_cfh@hotmail.com>
2017-04-25 01:05:21 -0300

    Delete actualiza.sh

--- Grupo06/comandos/actualiza.sh
+++
@@ -1,6 +0,0 @@
-cp -ar codigo instalacion/Grupo10
-rm -rf bin_backup
-mv instalacion/Grupo10/bin bin_backup
-mv instalacion/Grupo10/codigo instalacion/Grupo10/bin
-echo "Copié los archivos, pisé Grupo10/bin"
-echo "Hice bin_backup por las dudas, pero no programes en bin. Igual no sé si programaste en bin, eh, yo hago el backup igual."
